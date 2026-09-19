7a6f6b2dd5973388ed40918e3f3d8acd37dedbe1
Sachin Kushwah <ersachinsingh1918@gmail.com>
2026-09-19 23:10:26 +0530

    Add array table program

---
+++ arrayTable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void printTable(int n)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d*%d=%d\n", n, i, n * i);
+    }
+    int main(){
+    
+        int n = 5;
+        printTable(n);
+        return 0;
+    }
+}
